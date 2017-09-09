ebf4e9d594667dcea8462d2880e5b4efa471edc3
seb-faull <seb_faull@hotmail.co.uk>
2017-09-09 22:01:58 +0100

    implemented the addition operator

--- main.js
+++ main.js
@@ -2,35 +2,15 @@ var a = 'Addition';
 var s = 'Subtraction';
 var d = 'Division';
 var m = "Multiplication";
-var firstNumber = 'first number';
-var secondNumber = 'second number';
+var calculator = prompt('Welcome to the Calulator. Let me just take some input from you. \nPlease choose the following: \n' + a + ' - a \n' + s + ' - s \n' + d + ' - d \n' + m + ' - m');
+var firstNumber = prompt('first number');
+var secondNumber = prompt ('second number');
 var calculator = true;
 
-if (calculator === true) {
-	prompt('Welcome to the Calulator. Let me just take some input from you. \nPlease choose the following: \n' + a + ' - a \n' + s + ' - s \n' + d + ' - d \n' + m + ' - m');
-} else {
-	alert('Calulator out of order.')
-}
-
 if (a !== true) {
-	prompt('Please enter your ' + firstNumber);
-	
+	firstNumber = parseFloat(firstNumber);
+	secondNumber = parseFloat(secondNumber);
+	alert(firstNumber + secondNumber);
 } else {
 	alert('You did not enter your first number.');
 }
-
-if (a !== true) {
-	prompt('Please enter your ' + secondNumber);
-	
-} else {
-	alert('You did not enter your second number.');
-}
-
-firstNumber = parseFloat(firstNumber);
-alert(firstNumber + secondNumber);
-
-
-
-
-
-
