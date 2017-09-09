dc62588447fd1e396754b28a178a3b94be6cdb92
seb-faull <seb_faull@hotmail.co.uk>
2017-09-09 19:49:32 +0100

    implemented add, subtract, divide and multiply math operator varibales

--- main.js
+++ main.js
@@ -1,1 +1,36 @@
-var calculator = prompt('Welcome to the Calulator. Let me just take some input from you')
+var a = 'Addition';
+var s = 'Subtraction';
+var d = 'Division';
+var m = "Multiplication";
+var firstNumber = 'first number';
+var secondNumber = 'second number';
+var calculator = true;
+
+if (calculator === true) {
+	prompt('Welcome to the Calulator. Let me just take some input from you. \nPlease choose the following: \n' + a + ' - a \n' + s + ' - s \n' + d + ' - d \n' + m + ' - m');
+} else {
+	alert('Calulator out of order.')
+}
+
+if (a !== true) {
+	prompt('Please enter your ' + firstNumber);
+	
+} else {
+	alert('You did not enter your first number.');
+}
+
+if (a !== true) {
+	prompt('Please enter your ' + secondNumber);
+	
+} else {
+	alert('You did not enter your second number.');
+}
+
+firstNumber = parseFloat(firstNumber);
+alert(firstNumber + secondNumber);
+
+
+
+
+
+
