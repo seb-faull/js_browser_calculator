6708221f6d452fe744a4d0e5ac13fbd6a3e48965
seb-faull <seb_faull@hotmail.co.uk>
2017-09-10 11:00:35 +0100

    Implemented a new operator: To the Power of.

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 var a = 'Addition';
 var s = 'Subtraction';
 var d = 'Division';
-var m = "Multiplication";
-var calculator = prompt('Welcome to the Calulator. Let me just take some input from you. \nPlease choose from the following: \n' + a + ' - a \n' + s + ' - s \n' + d + ' - d \n' + m + ' - m');
+var m = 'Multiplication';
+var p = 'To the Power of';
+
+var calculator = prompt('Welcome to the Calulator. Let me just take some input from you. \nPlease choose from the following: \n' + a + ' - a \n' + s + ' - s \n' + d + ' - d \n' + m + ' - m \n' + p + ' - p');
 var firstNumber = prompt('First number, please');
 var secondNumber = prompt ('Second number, please');
 
@@ -26,6 +28,10 @@ if (calculator === "a") {
 	firstNumber = parseFloat(firstNumber);
 	secondNumber = parseFloat(secondNumber);
 	alert(firstNumber * secondNumber);
+} else if (calculator === "p") {
+	firstNumber = parseFloat(firstNumber);
+	secondNumber = parseFloat(secondNumber);
+	alert(firstNumber ** secondNumber);
 } else {
 	alert('You did not insert a number or a valid math operator!');
 }
